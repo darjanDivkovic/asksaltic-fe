3701103e486c9025f75078ee17ae7f34f91a863c
Darjan <daarjan.tz@gmail.com>
2024-06-19 00:19:39 +0200

    Final update for now

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='flex w-full justify-between py-10 lg:py-20 relative z-10 items-center'>
+      <div className='flex w-full justify-center lg:justify-between  py-10 lg:py-20 relative z-10 items-center'>
         <div ref={logoRef}>
           <Image src="/logo.png" alt="Logo" width={76} height={76} />
         </div>
@@ -61,21 +61,21 @@ const HomePage = () => {
 
       <div className='flex flex-col relative z-10 mt-[3%]'>
 
-        <h1 className='text-[10vw] sm:text-[8rem] font-custom leading-none sm:leading-[8rem] font-bold drop-shadow-2xl filter-none'>
+        <h1 className='text-[10vw] sm:text-[8rem] font-custom leading-none sm:leading-[8rem] font-bold drop-shadow-2xl filter-none text-center lg:text-left'>
           ASK<br />
           <span className='text-[#71673E] '>SALTIC</span><br />
           TUZLA
         </h1>
 
 
-        <p className='text-[3vw] sm:text-[15px] font-extralight opacity-80 mt-8 w-full sm:w-[55%]'>
+        <p className='text-[3vw] sm:text-[15px] font-extralight opacity-80 mt-8 w-full sm:w-[55%] text-center lg:text-left'>
           Dobrodošli na službeni website Airsoft Kluba Saltic Tuzla!<br /> Pridružite nam se u avanturama punim adrenalina, strategije i timskog duha.<br /><br /> Bilo da ste iskusni igrač ili početnik, kod nas ćete pronaći nezaboravna iskustva, prijateljstva i izazove koji će vas motivirati.
           <strong className='underline pl-2'>Uskoro sve informacije o članstvima, timu, susretima i više na ovom sajtu...</strong>
         </p>
       </div>
 
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-40"
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-40 shadow-inner"
         autoPlay
         muted
         loop
